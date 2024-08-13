f35da6cb523cfea7738ef8ed51013caef66ca8db
Judgy53 <9182@holbertonstudents.com>
2024-08-13 10:56:15 +0200

    webpack - task 0: Basic setup

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$('body').append([
+  $('<p>').text('Holberton Dashboard'),
+  $('<p>').text('Dashboard data for the students'),
+  $('<p>').text('Copyright - Holberton School'),
+]);
