fd9e153841aedcf34b614120b5b700d6ffd4e74c
Judgy53 <9182@holbertonstudents.com>
2024-08-14 15:21:31 +0200

    webpack - task 2 webpack.config formatting

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,35 +1,25 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public")
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true,
-              disable: true,
-            },
-          }
-        ],
+        use: ['file-loader', 'image-webpack-loader'],
       },
     ],
   },
   performance: {
-    maxAssetSize: 1000000,
-    maxEntrypointSize: 1000000,
+    hints: false
   },
 };
