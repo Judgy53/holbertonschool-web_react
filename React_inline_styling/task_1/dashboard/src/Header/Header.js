b17d51c9954801b3fb8ab303e377cc827aa0abbf
Judgy53 <9182@holbertonstudents.com>
2024-09-03 11:55:11 +0200

    React_inline_styling - Task 1: Aphrodite

---
+++ React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -0,0 +1,29 @@
+import logo from '../assets/holberton-logo.jpg';
+import { StyleSheet, css } from 'aphrodite';
+
+function Header() {
+  return (
+    <div className={css(styles.header)}>
+      <img src={logo} className={css(styles.logo)} alt="logo" />
+      <h1>
+        School dashboard
+      </h1>
+    </div>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottom: '3px solid #e0354b',
+    fontWeight: 'bold',
+    color: '#e0354b',
+  },
+  logo: {
+    height: '200px',
+  }
+});
+
+export default Header;
