4e1dbc3ea00f9b9a62a0eaf0edc730eb655635b9
Judgy53 <9182@holbertonstudents.com>
2024-09-13 10:38:29 +0200

    react_state - Task 2: Context

---
+++ react_state/task_2/dashboard/src/App/App.test.js
@@ -0,0 +1,91 @@
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('<App />', () => {
+  let wrapper;
+
+  describe('isLoggedIn = false', () => {
+    beforeEach(() => {
+      wrapper = shallow(<App />);
+    });
+
+    it('renders without crashing', () => { });
+
+    it('contains the Notifications component', () => {
+      expect(wrapper.find('Notifications')).toHaveLength(1);
+    });
+
+    it('contains the Header component', () => {
+      expect(wrapper.find('Header')).toHaveLength(1);
+    });
+
+    it('contains the Footer component', () => {
+      expect(wrapper.find('Footer')).toHaveLength(1);
+    });
+
+    it('contains the Login component', () => {
+      expect(wrapper.find('Login')).toHaveLength(1);
+    });
+
+    it('does not contain the CourseList component', () => {
+      expect(wrapper.find('CourseList')).toHaveLength(0);
+    });
+  });
+
+  describe('isLoggedIn = true', () => {
+    beforeEach(() => {
+      wrapper = shallow(<App />);
+      wrapper.instance().logIn('a@a.com', 'azerty1234');
+    });
+
+    it('does not contain the Login component', () => {
+      expect(wrapper.find('Login')).toHaveLength(0);
+    });
+
+    it('contains the CourseList component', () => {
+      expect(wrapper.find('CourseList')).toHaveLength(1);
+    });
+  });
+
+  describe('logOut event', () => {
+    it('calls logOut prop', () => {
+      const logOutMock = jest.fn();
+      const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => {});
+
+      const wrapper = shallow(<App />);
+      wrapper.setState({ logOut: logOutMock });
+
+      window.dispatchEvent(new KeyboardEvent('keydown', {
+        key: 'h',
+        ctrlKey: true,
+      }));
+
+      expect(logOutMock).toBeCalled();
+      expect(alertSpy).toBeCalledWith('Logging you out');
+
+      alertSpy.mockRestore();
+    });
+  });
+
+  describe('state.displayDrawer', () => {
+    beforeEach(() => {
+      wrapper = shallow(<App />);
+    });
+
+    it('has default value set to false', () => {
+      expect(wrapper.state('displayDrawer')).toBe(false);
+    });
+
+    it('changes value to true when handleDisplayDrawer() is called', () => {
+      wrapper.instance().handleDisplayDrawer();
+      expect(wrapper.state('displayDrawer')).toBe(true);
+    });
+
+    it('changes value to false when handleHideDrawer() is called', () => {
+      wrapper.setState({displayDrawer: true});
+      wrapper.instance().handleHideDrawer();
+
+      expect(wrapper.state('displayDrawer')).toBe(false);
+    });
+  });
+});
---
+++ react_state/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,148 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import NotificationItem from './NotificationItem';
+import NotificationItemShape from './NotificationItemShape';
+import { StyleSheet, css } from 'aphrodite';
+
+class Notifications extends React.Component {
+  static propTypes = {
+    displayDrawer: PropTypes.bool,
+    listNotifications: PropTypes.arrayOf(PropTypes.shape(NotificationItemShape)),
+    handleDisplayDrawer: PropTypes.func,
+    handleHideDrawer: PropTypes.func
+  }
+
+  static defaultProps = {
+    displayDrawer: false,
+    listNotifications: [],
+    handleDisplayDrawer: () => { },
+    handleHideDrawer: () => { },
+  }
+
+  shouldComponentUpdate(nextProps) {
+    const { listNotifications, displayDrawer } = this.props;
+    return (
+      nextProps.listNotifications.length > listNotifications.length
+      || nextProps.displayDrawer != displayDrawer
+    );
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  onClickCloseBtn() {
+    console.log('Close button has been clicked');
+  }
+
+  render() {
+    const { listNotifications, displayDrawer, handleDisplayDrawer, handleHideDrawer } = this.props;
+
+    const notificationItems = listNotifications.map(item =>
+      <NotificationItem
+        type={item.type}
+        value={item.value}
+        html={item.html}
+        key={item.id}
+        markAsRead={this.markAsRead.bind(this, item.id)}
+      />
+    );
+
+    return (
+      <>
+        {displayDrawer
+          ? <div className={css(styles.notifications)}>
+            <button type="button" aria-label="Close" className={css(styles.closeBtn)} onClick={handleHideDrawer}>
+              x
+            </button>
+            <ul className={css(styles.ul)}>
+              {notificationItems.length
+                ? <><p>Here is the list of notifications</p>{notificationItems}</>
+                : <NotificationItem type="default" value="No new notification for now" />
+              }
+            </ul>
+          </div>
+          : <div className={css(styles.menuItem)} aria-label="Open" onClick={handleDisplayDrawer}>Your notifications</div>
+        }
+      </>
+    );
+  }
+}
+
+const opacityAnimation = {
+  'from': {
+    opacity: 0.5
+  },
+  'to': {
+    opacity: 1
+  }
+}
+
+const bouncingAnimation = {
+  '0%': {
+    transform: 'translateY(0px)'
+  },
+  '15%': {
+    transform: 'translateY(-5px)'
+  },
+  '45%': {
+    transform: 'translateY(5px)'
+  },
+  '60%': {
+    transform: 'translateY(0px)'
+  },
+}
+
+const styles = StyleSheet.create({
+  menuItem: {
+    position: 'absolute',
+    right: '1rem',
+    backgroundColor: '#fff8f8',
+    cursor: 'pointer',
+    ':hover': {
+      animationName: [opacityAnimation, bouncingAnimation],
+      animationDuration: '1s, .5s',
+      animationIterationCount: '3'
+    }
+  },
+  notifications: {
+    border: '2px dotted #e0354b',
+    padding: '1rem .5rem',
+    position: 'absolute',
+    right: '1rem',
+    top: '2rem',
+    minwWidth: '33vw',
+    '@media (max-width: 900px)': {
+      inset: 0,
+      width: '100%',
+      height: '100%',
+      border: 'none',
+      padding: 0,
+      backgroundColor: 'white'
+    }
+  },
+  ul: {
+    '@media (max-width: 900px)': {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+      fontSize: '20px',
+    }
+  },
+  closeBtn: {
+    position: 'absolute',
+    top: '.5rem',
+    right: 0,
+    background: 'none',
+    border: 'none',
+    fontWeight: 'bold',
+    fontSize: '15pt',
+    cursor: 'pointer',
+    '@media (max-width: 900px)': {
+      top: 0,
+      right: '.5rem'
+    }
+  }
+});
+
+export default Notifications;
